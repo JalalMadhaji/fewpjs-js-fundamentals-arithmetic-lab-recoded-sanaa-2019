b030ef6d93f2d5273ec3e07f254d4597f3068668
Jalal Al-Madhaji <jalal.almadhaji90@gmail.com>
2019-10-09 09:51:21 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,14 +2,8 @@ let num1 = 2;
 let num2 = 31;
 let multiply = num1 * num2;
 
-console.log(multiply);
-
-
 let random = Math.floor(Math.random() * 10) + 1;
-console.log(random);
 
 let mod = 4;
 
 let max = Math.max((10,3,5,7,20));
-
-console.log(max);
